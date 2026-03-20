115492ef578b7f7a8b10ae289250033692ed81d4
lokajay <lokajaybhadra04@gmail.com>
2026-03-20 07:52:59 +0530

    new update project

--- Majorproject/app.js
+++ Majorproject/app.js
@@ -283,6 +283,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
 
+  
 
   res.status(statusCode).render("error", { err: { statusCode, message } });
 });
