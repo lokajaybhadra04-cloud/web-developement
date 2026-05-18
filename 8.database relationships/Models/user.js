5f4c20a216e2573bf0332aa8a3f6cef7b5439fd9
lokajay <lokajaybhadra04@gmail.com>
2026-05-18 20:05:00 +0530

    update

---
+++ 8.database relationships/Models/user.js
@@ -0,0 +1,63 @@
+//connect db
+const mongoose = require("mongoose");
+
+main().then(()=> console.log("connection sucessful"))
+      .catch(err => console.log(err) );
+
+async function  main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/relationshipDemo');
+
+}
+
+
+
+
+//create schema
+
+const {Schema}=mongoose;
+
+
+
+const userSchema = new Schema({
+    username:String,
+    addresses:[
+        {   _id: false,
+            location:String,
+            city:String,
+        },
+    ],
+  });
+
+const {model}=mongoose;
+
+const User=model("User",userSchema);
+
+
+
+//add user function
+
+const addUsers=async()=>{
+let user1= new User({username:"lokajay",
+    addresses:[{
+                location:"mistry patti",
+                city:"guwahati"
+                 }
+
+             ],
+})
+
+console.log(user1);
+
+user1.addresses.push({  location:"shantinikton",
+                city:"shiliong"})
+
+console.log(user1);
+
+
+//add user to db
+let result=await user1.save();
+console.log(result);
+}
+
+
+addUsers();
